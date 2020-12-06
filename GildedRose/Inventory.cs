a5bc816680206be9f099a0fcf4c5e58cae38dbc3
pavolhodas <pavolhodas4@gmail.com>
2020-12-06 18:46:31 +0100

    Implement the new features of the items

--- GildedRose/Inventory.cs
+++ GildedRose/Inventory.cs
@@ -5,7 +5,6 @@ namespace GildedRose
     class Inventory
     {
         private readonly IEnumerable<Item> items;
-
         public Inventory(IEnumerable<Item> items)
         {
             this.items = items;
@@ -20,10 +19,89 @@ namespace GildedRose
         /// "Backstage passes to a TAFKAL80ETC concert"
         /// "Conjured Mana Cake"
         /// </summary>
+        /// 
+
+
+
         public void UpdateQuality()
         {
             // TODO ...
             // Hint: Iterate through this.items and check Name property to access specific item
+            foreach(var item in items)
+            {
+                
+
+                if (item.Name.Contains("Aged Brie"))
+                {
+                    item.SellIn = item.SellIn - 1;
+                    item.Quality = item.Quality + 1;
+
+                    if (item.SellIn == -1)
+                    {
+                        item.Quality = item.Quality - 1;
+                    }
+
+                    if (item.Quality <= 0)
+                    {
+                        item.Quality = 2;
+                    }
+                }
+
+                    if (item.Name.Contains("Backstage passes"))
+                    {
+                        item.SellIn = item.SellIn - 1;
+                        item.Quality = item.Quality + 1;
+
+                            if(item.SellIn <= 10)
+                            {
+                                item.Quality = item.Quality + 1;
+                            }
+                                if (item.SellIn <= 5)
+                                {
+                                    item.Quality = item.Quality + 1;
+
+                                }
+                        if (item.SellIn <= 0)
+                        {
+                            item.Quality = 0;
+                        }
+                    }
+
+                        if (item.Name.Contains("Conjured"))
+                        {
+                            item.SellIn = item.SellIn - 1;
+                            item.Quality = item.Quality - 2;
+
+                            if (item.SellIn == -1)
+                            {
+                                item.Quality = item.Quality - 2;
+
+
+                            }
+                                if (item.Quality <= 0)
+                                {
+                                    item.Quality = 0;
+                                }
+                        }
+
+                
+                            if (item.Name.Contains("Elixir") || item.Name.Contains("Dexterity Vest"))
+                            {
+                        
+                                item.SellIn = item.SellIn - 1;
+                                item.Quality = item.Quality - 1;
+
+                                if (item.SellIn == -1)
+                                {
+                                    item.Quality = item.Quality - 1;
+                                }
+                                    if (item.Quality <= 0)
+                                    {
+                                        item.Quality = 0;
+                                    }
+
+                            }
+            }
         }
     }
 }
